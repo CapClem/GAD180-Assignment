475706dcad2130682666d165b88fdd1c3d350bbe
Jacob <1014906@student.sae.edu.au>
2019-07-02 12:03:03 +1000

    added cam and greyboxing
    As title

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform target1;
+    public Transform target2;
+
+    public enum CameraStyle
+    {
+        fixedPointAngleFollow,
+        fixedAngleMoveFollow
+
+    };
+    public CameraStyle cameraStyle;
+
+
+
+    private Vector3 targetMidPoint;
+    private Vector3 initialTargetMidPoint;
+    private Vector3 initialCamPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // These are used to move the cam relative to the mid point while keeping what ever offset there was to start with.
+        initialTargetMidPoint = (target1.position + (target2.position - target1.position) / 2);
+        initialCamPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The following formula is used to find the mid point between two points  A + (B - A) / 2;
+
+        targetMidPoint = (target1.position + (target2.position - target1.position) / 2);
+
+
+        if (cameraStyle == CameraStyle.fixedPointAngleFollow)
+        {
+            transform.LookAt(targetMidPoint);
+
+        }
+        else if (cameraStyle == CameraStyle.fixedAngleMoveFollow)
+        {
+            transform.position = initialCamPosition + (targetMidPoint - initialTargetMidPoint);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position,targetMidPoint,-(Vector3.Distance(target1.position,target2.position))/3);
+
+        /* To do----- 
+        Zoom to keep players in view.
+        this will need to zoom based on the distance between each player,
+        relative to the distance between the player Mid point and the camera.
+
+        Desireable.
+        Maybe also use the script to put the camera into a good starting position.
+
+        Out of scope.
+        maybe make a roaming camera that moves around and rotates to both keep both players in view 
+        and avoid obstacles between the camera and players.
+         */
+
+    }
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,18 +6,25 @@ public class PlayerControl : MonoBehaviour
 {
 
     //controller stuff
+    public enum ControlScheme
+    {
+        DS4,
+        XBOX,
+        Keyboard,
+    };
+    public ControlScheme myControlScheme;
+    /// <summary>
+    /// Fire,move,aim,reloading,jumping,pickup,drop, Menu nav.
+    /// </summary>
     private string leftVertical, leftHorizontal,rightVertical,rightHorizontal,
         dPadVertical,dpadHorizontal,
-        leftTriggerAxis,rightTriggerAxis,leftJoyButton,rightJoyButton,
-        xButton,oButton,squareButton,triangleButton,leftTriggerButton,rightTriggerButton
-        ,shareButton,optionButton,psButton;
+        jumpButton,oButton,reloadButton,PickupButton,alternateFireButton,fireButton
+        ,shareButton,optionButton,pauseButton;
     public int controllerNumber = 1;
-
 
     // game components
     Rigidbody rb;
     CharacterController charCtrl;
-
 
     //RotatedHeadings
     private Vector3 forward, right;
@@ -30,68 +37,91 @@ public class PlayerControl : MonoBehaviour
     //Gravity
     public float gravity = 10.0f;
 
-
-    bool lastInput = false;
+    //bool lastInput = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        setUpInputs(controllerNumber, "DS4");
+        //this takes a controller number and a ControlScheme
+        setUpInputs(controllerNumber, myControlScheme);
 
         //GetComponents
         rb = this.GetComponent<Rigidbody>();
         charCtrl = this.GetComponent<CharacterController>();
 
         //headings
+        // this is where we rotate the players directions to isometric
         forward = Quaternion.Euler(0, 45, 0) * this.transform.forward;
         right = Quaternion.Euler(0, 90, 0) * forward;
 
     }
 
-    void setUpInputs(int Number, string CtrlrType)
+    void setUpInputs(int Number, ControlScheme selectedControlScheme)
     {
-        if (CtrlrType == "DS4")
+        switch (selectedControlScheme)
         {
-            //left thumbstick
-            leftVertical = "J" + Number + "LVertical";
-            leftHorizontal = "J" + Number + "LHorizontal";
+            case ControlScheme.DS4:
+                //left thumbstick
+                leftVertical = "J" + Number + "LVertical";
+                leftHorizontal = "J" + Number + "LHorizontal";
 
-            //right thumbstick
-            rightVertical = "J" + Number + "RVertical";
-            rightHorizontal = "J" + Number + "RHorizontal";
+                //right thumbstick
+                rightVertical = "J" + Number + "RVertical";
+                rightHorizontal = "J" + Number + "RHorizontal";
 
-            //left trigger
-            leftTriggerAxis = "J" + Number + "LTriggerAxis";
-            leftTriggerButton = "J" + Number + "LTriggerButton";
+                //dpad
+                dPadVertical = "J" + Number + "DPadVertical";
+                dpadHorizontal = "J" + Number + "DPadHorizontal";
 
-            //right trigger
-            rightTriggerAxis = "J" + Number + "RTriggerButton";
-            rightTriggerButton = "J" + Number + "RTriggerButton";
+                //Buttons
+                alternateFireButton = "J" + Number + "LTriggerButton";
+                fireButton = "J" + Number + "RTriggerButton";
+                jumpButton = "J" + Number + "XButton";
+                //oButton = "J" + Number + "OButton";
+                PickupButton = "J" + Number + "TriangleButton";
+                reloadButton = "J" + Number + "SquareButton";
+                pauseButton = "J" + Number + "optionsButton";
+                break;
 
-            //dpad
-            dPadVertical = "J" + Number + "DPadVertical";
-            dpadHorizontal = "J" + Number + "DPadHorizontal";
+            case ControlScheme.XBOX:
+                // leaving this blank for some else to try out.
+                break;
 
-            //Buttons
-            leftTriggerButton = "J" + Number + "LTriggerButton";
-            rightTriggerButton = "J" + Number + "RTriggerButton";
-            xButton = "J" + Number + "XButton";
-            oButton = "J" + Number + "OButton";
-            triangleButton = "J" + Number + "TriangleButton";
-            squareButton = "J" + Number + "SquareButton";
+            case ControlScheme.Keyboard:
+                //left thumbstick
+                leftVertical = "KVertical";
+                leftHorizontal = "KHorizontal";
+
+                //right thumbstick
+                rightVertical = "MVertical";
+                rightHorizontal = "MHorizontal";
+
+                //dpad
+                dPadVertical = "KVertical";
+                dpadHorizontal = "KHorizontal";
+
+                //Buttons
+                alternateFireButton = "LMouse";
+                fireButton = "RMouse";
+                jumpButton = "SpaceBar";
+                PickupButton = "EButton";
+                reloadButton = "RButton";
+                pauseButton = "Esc";
+                break;
+
+            default:
+                Debug.Log("Control Scheme was Not Set.");
+                break;
         }
-
     }
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetButton(triangleButton) && charCtrl.isGrounded)
+        // checks for jump button, and wether or not the player is on the ground, If so, It adds to the move direction.
+        if (Input.GetButton(jumpButton) && charCtrl.isGrounded)
         {
             moveDir.y = jumpSpeed;
         }
-
-
 
         if (Mathf.Abs(Input.GetAxis(leftVertical)) > stickFilter || Mathf.Abs(Input.GetAxis(leftHorizontal)) > stickFilter)
         {
@@ -99,7 +129,7 @@ public class PlayerControl : MonoBehaviour
             Vector3 horizontalMovement = right * Input.GetAxis(leftHorizontal);
             //moveDir=((verticalMovement + horizontalMovement) * speed);
             charCtrl.Move(((verticalMovement + horizontalMovement) * speed) * Time.deltaTime);
-            Debug.Log(" Character moved" + (verticalMovement + horizontalMovement));
+            // Debug.Log(" Character moved" + (verticalMovement + horizontalMovement));
         }
         moveDir.y -= gravity * Time.deltaTime;
         charCtrl.Move(moveDir * Time.deltaTime);
